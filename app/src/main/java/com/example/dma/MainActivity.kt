570b3d8dd4983a3258361035df9216d56668097a
hikki.HIN <hikki.hin@gmail.com>
2023-09-19 17:20:04 +0700

    Создание проекта, лаб.раб №1

---
+++ app/src/main/java/com/example/dma/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dma
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val iMainAct_lab1 = Intent(this, MainActivity_Lab1::class.java)
+        val button_nextAct_lab1: Button = findViewById(R.id.button)
+        button_nextAct_lab1.setOnClickListener{
+            startActivity(iMainAct_lab1)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/dma/MainActivity_Lab1.kt
@@ -0,0 +1,49 @@
+package com.example.dma
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.sqrt
+
+class MainActivity_Lab1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lab1)
+
+
+        val button_back: Button = findViewById(R.id.button2)
+        button_back.setOnClickListener{
+            finish()
+        }
+
+
+        val n1: EditText = findViewById(R.id.editTextText)
+        val n2: EditText = findViewById(R.id.editTextText2)
+        val n3: EditText = findViewById(R.id.editTextText3)
+        val c1: EditText = findViewById(R.id.editTextText4)
+        val button_calc: Button = findViewById(R.id.button3)
+        val resultText1 = findViewById<TextView>(R.id.textView4)
+
+        button_calc.setOnClickListener{
+            val c1Char = c1.text.toString().trim()
+            val n1N = n1.text.toString().trim().toFloat()
+            val n2N = n2.text.toString().trim().toFloat()
+            val n3N = n3.text.toString().trim().toFloat()
+            if (c1Char == "A"){
+                resultText1.text = ((n1N+n2N+n3N)/3).toString()
+            }
+            else if (c1Char == "G"){
+                resultText1.text = sqrt(n1N*n2N*n3N).toString()
+            }
+            else{
+                resultText1.text = "Ошибка. Введен неверный символ."
+                Toast.makeText(this, "Введен неизвестный символ", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
